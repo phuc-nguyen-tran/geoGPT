3d131268718022f8410d360b243053990cd9ff91
nguy3ntrann <phucnguyentran79@gmail.com>
2023-09-04 16:23:54 -0400

    clean files

--- client/src/components/MyMap.js
+++ client/src/components/MyMap.js
@@ -1,12 +1,41 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+//import axios from "axios";
 import { MapContainer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import countries from "./../data/countries.json";
-import { Icon } from "leaflet";
 
 function MyMap() {
-  const [placeChosen, setPlaceChosen] = useState("")
+  const [place, setPlace] = useState("");
+  const [message, setMessage] = useState(""); // State to store the message
+
+  const getMessage = async (clickedCountryName) => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        placeChosen:  clickedCountryName , // Use the clickedCountryName
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const response = await fetch("http://localhost:5000/generate", options);
+      const data = await response.json();
+      setMessage(data.choices[0].message.content); // Update the message state
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  useEffect(() => {
+    //console.log(place);
+    // Call getMessage() when place changes, with a callback
+    if (place) {
+      console.log(place)
+      getMessage(place);
+    }
+  }, [place]);
+
   const countryStyle = {
     fillColor: "pink",
     fillOpacity: 0.3,
@@ -24,7 +53,7 @@ function MyMap() {
         fillOpacity: 1,
         color: "black",
         weight: 2,
-      })
+      });
     });
     layer.on("mouseout", function (e) {
       this.closePopup();
@@ -32,20 +61,22 @@ function MyMap() {
         fillColor: "pink",
         fillOpacity: 0.3,
         color: "black",
-        weight: 0.7
-      })
+        weight: 0.7,
+      });
     });
-    layer.on("click", async function(e){
-      setPlaceChosen(countryName);
-    })
+    layer.on("click", function (e) {
+      const clickedCountryName = country.properties.ADMIN;
+      //console.log("Clicked country:", clickedCountryName);
+      setPlace(clickedCountryName);
+    });
   };
- 
+
   return (
     <div>
       <MapContainer
-        style={{ height: "100vh" }}
+        style={{ height: "90vh" }}
         zoom={1.5}
-        center={[39.9334,32.8597]}
+        center={[39.9334, 32.8597]}
         scrollWheelZoom={false}
       >
         <GeoJSON
@@ -54,6 +85,7 @@ function MyMap() {
           onEachFeature={onEachCountry}
         />
       </MapContainer>
+      {message && <p>{message}</p>}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
-import express from "express"
-import cors from "cors"
-import generate from "./generate.js";
+import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 
 const app = express();
@@ -8,25 +7,46 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT;
+const port = 5000;
 
-app.get("/", async(req,res) => {
-    res.json("Hello World!")
-})
+const API_KEY = "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";
 
-app.post("/generate", async(req,res) => {
-    const placeChosen = req.body.placeChosen;
-    try{
-        const travelPlan = await generate(placeChosen);
-        res.json({response: travelPlan});
-    } catch(err){
-        console.log(err);
-    }
-})
+app.get("/", async (req, res) => {
+  res.json("Hello World!");
+});
+
+app.post("/generate", async (req, res) => {
+  const placeChosen = req.body.placeChosen;
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Generate a travel plan in ${placeChosen} in 7 days`,
+        },
+      ],
+      max_tokens: 2000,
+      temperature: 0.3,
+    }),
+  };
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      options
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(port, () => {
-    console.log(`App is listening on port ${port}`);
-})
-
-
-
+  console.log(`App is listening on port ${port}`);
+});
